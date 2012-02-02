b74b282ae6db6283b746f69b90ba54457415287c
mohan <mohan@ZION.(none)>
2012-02-02 19:25:12 +0530

    Added test case and test data file for use case of multiple non-contiguous hyphens. This test case currently fails.

---
+++ tests/pathological/test_multiple_non-contiguous_hyphens.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$1" == "-d" ]
+then
+    echo "Test name a-b-c.ext "
+    exit 0
+fi
+
+cd test-data-files
+result=`../../rename a-b-c.mp4`
+if [ "$result" == "a-b.mp4" ]
+then
+#    echo success
+    exit 0
+else
+#    echo problem
+    exit 1
+fi
