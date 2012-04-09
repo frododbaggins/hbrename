6e0fa8b04792519bbd84ca1a3413b8b097897938
Mohan Nayaka <mohansnayaka@gmail.com>
2012-04-09 17:19:11 +0530

    Small fixup to 3cf4a (support for debug code)

--- match.pl
+++ match.pl
@@ -13,6 +13,12 @@ if ( $#ARGV != 0 )
     print_usage ();
 }
 
+my $is_debug=0;
+if ( $0 =~ m/match.pl/ )
+{
+    $is_debug=1;
+}
+
 my $in_filename=$ARGV[0];
 $in_filename =~ m/([a-zA-Z0-9]*)(_{0,})-([a-zA-Z0-9]+)\.([a-zA-Z0-9]+)/;
 
@@ -20,13 +26,16 @@ $in_filename =~ m/([a-zA-Z0-9]*)(_{0,})-([a-zA-Z0-9]+)\.([a-zA-Z0-9]+)/;
 # described in this if clause 
 # FIXME: Reverse this condition, so that debug code runs _unless_ the name
 #        is rename or .rename
-if ( $0 =~ m/match.pl/)
+if ( $is_debug )
 {
 #   DEBUG code goes here ...
+    print $1 . "\n";
+    print $2 . "\n";
+    print $3 . "\n";
+    print $4 . "\n";
 }
 
 if ((defined $1) && (defined $4))
 {
     print "$1.$4";
 }
-
