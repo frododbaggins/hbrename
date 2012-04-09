3cf4a8b525888b686add0d6e2029a996219fca1d
Mohan Nayaka <mohansnayaka@gmail.com>
2012-04-09 16:58:14 +0530

    Add support for debug code

--- match.pl
+++ match.pl
@@ -14,5 +14,19 @@ if ( $#ARGV != 0 )
 }
 
 my $in_filename=$ARGV[0];
-$in_filename =~ m/([a-zA-Z0-9]+)_{0,}-([a-zA-Z0-9]+)\.([a-zA-Z0-9]+)/;
-print "$1.$3";
+$in_filename =~ m/([a-zA-Z0-9]*)(_{0,})-([a-zA-Z0-9]+)\.([a-zA-Z0-9]+)/;
+
+# This is to aid debugging : when run as rename/.rename, do not take debug actions
+# described in this if clause 
+# FIXME: Reverse this condition, so that debug code runs _unless_ the name
+#        is rename or .rename
+if ( $0 =~ m/match.pl/)
+{
+#   DEBUG code goes here ...
+}
+
+if ((defined $1) && (defined $4))
+{
+    print "$1.$4";
+}
+
