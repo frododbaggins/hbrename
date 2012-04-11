6cdf3ff8157701157aa266c6b6a0a02908fc7d3b
Mohan Nayaka <mohansnayaka@gmail.com>
2012-04-11 16:04:40 +0530

    edit error message for a_b_c_d-e.mp4 test case

--- tests/basic/test_uscore3.sh
+++ tests/basic/test_uscore3.sh
@@ -14,6 +14,6 @@ then
     exit 0
 else
 #    echo problem
-    echo "    Expected result : filename.mp4 Actual result: $result " > $error_log_file
+    echo "    Expected result : a_b_c_d.mp4 Actual result: $result " > $error_log_file
     exit 1
 fi
