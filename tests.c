616a80bb27cde238780fc51489ed67bb9a7e2275
Mohan Nayaka <mohansnayaka@gmail.com>
2012-05-16 18:56:42 +0530

    Makeshift facility to test single names

--- tests.c
+++ tests.c
@@ -3,11 +3,24 @@ extern char *newname;          /* From libcleanup.so */
 
 int main(int argc, char **argv)
 {
-    int opt, verbose;
-    while ((opt = getopt(argc, argv, "v")) != -1) {
+    int opt, verbose = 0, cmdcheck = 0;
+    char *cmdarg;
+    while ((opt = getopt(argc, argv, "vc:")) != -1) {
         switch (opt) {
         case 'v':
             verbose = 1;
+            break;
+        case 'c':
+            if(strcmp(optarg, "")) {
+                // truncate to NAMELEN (80) chars when computing new name
+                    cmdarg = malloc(NAMELEN);
+                    strncpy(cmdarg, optarg, NAMELEN-1);
+                    strcat(cmdarg,"\0");
+                    cmdcheck = 1;
+            } else {
+                printf ("-c option requires an argument\n");
+                return -1;
+            }
             break;
         default:
             verbose = 0;
@@ -15,6 +28,12 @@ int main(int argc, char **argv)
     }
     assert ((verbose == 0) || (verbose == 1));
     newname = malloc(NAMELEN * sizeof(char));
+    if (cmdcheck) {
+        printf("%s\n", new_name(cmdarg));
+        free (cmdarg);
+        free(newname);
+        return 0;
+    }
     run_tests(verbose);
     free(newname);
     return 0;
