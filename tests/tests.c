d2608b3c6a8b2b09f685d5c677909e254aedf41b
Mohan Nayaka <mohansnayaka@gmail.com>
2012-06-11 11:35:48 +0530

    Merge branch 'new-and-improved!' into trivial
    * new-and-improved!:
      Major change in hyphen removal behaviour
      Document new_name routine
      Enable setting check type from runtests command line
    
    Conflicts: <=========Resolved manually
    	tests/tests.c

--- tests/tests.c
+++ tests/tests.c
@@ -3,9 +3,9 @@ extern char *newname;          /* From libcleanup.so */
 
 int main(int argc, char **argv)
 {
-    int opt, verbose = 0, cmdcheck = 0;
+    int opt, verbose = 0, cmdcheck = 0,soft_hard = 0;
     char *cmdarg;
-    while ((opt = getopt(argc, argv, "vc:")) != -1) {
+    while ((opt = getopt(argc, argv, "shvc:")) != -1) {
         switch (opt) {
         case 'v':
             verbose = 1;
@@ -25,6 +25,12 @@ int main(int argc, char **argv)
         case '?':
             fprintf (stderr, "Unexpected option found - exiting\n");
             exit (-1);
+        case 's':
+            soft_hard = 0;
+            break;
+        case 'h':
+            soft_hard = 1;
+            break;
         default:
             verbose = 0;
         }
@@ -37,6 +43,7 @@ int main(int argc, char **argv)
         free(newname);
         return 0;
     }
+    set_check_type (soft_hard);
     int tests_status = run_tests(verbose);
     free(newname);
     return tests_status;
