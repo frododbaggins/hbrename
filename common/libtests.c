8d43f1754c8724248300325ecabcefa1ce683f64
Mohan Nayaka <mohansnayaka@gmail.com>
2012-06-19 11:22:17 +0530

    Code cleanup to facilitate testing of cleanup tool
    Preparation for fix of github issue #5.

--- common/libtests.c
+++ common/libtests.c
@@ -1,32 +1,40 @@
 #include "libtests.h"
 
+typedef int bool;
+#define IGNORE 2
+#define TRUE 1
+#define FALSE 0
+
 int test_count = 0, failcount = 0, check_type = 0;
+struct test_data_bufs
+{
+    char buf_old [NAMELEN],
+         buf_exp [NAMELEN];
+};
+struct test_data_bufs bufs;
 
 void set_check_type (int type)
 {
     assert ((type == SOFT_CHECK) || (type == HARD_CHECK));
     check_type = type;
 }
-int check (char * filename)
-{
-    assert (filename);
-    FILE * fp = fopen (filename, "r");
-    if (NULL == fp) {
-        return -1;
-    }
 
+bool get_single_entry (FILE * fp)
+{
+    assert ((FILE *) -1 != fp);
     char * line = calloc (sizeof (char), 2 * NAMELEN);
     size_t dummy = 0;
     ssize_t read;
-    char buf_old [NAMELEN] = "", buf_exp [NAMELEN] = "";
+    //    char buf_old [NAMELEN] = "", buf_exp [NAMELEN] = "";
 
-    while ((read = getline (&line, &dummy, fp)) != -1) {
+    if ((read = getline (&line, &dummy, fp)) != -1) {
         char *ptr;
         int i = 0;
 
         if (*line == '#') {
             /* Ignore comment lines */
-            continue;
+            bufs.buf_old[0] = '#';
+            return IGNORE;
         }
         d_printf ("-----------------------------------------\n");
         d_printf ("Got %s len = %d\n", line, strlen (line));
@@ -38,41 +46,62 @@ int check (char * filename)
 
         /* Clear buffers from last iteration */
         for (i = 0; i < NAMELEN; i++) {
-            buf_old [i] = buf_exp [i] = 0;
+            bufs.buf_old [i] = bufs.buf_exp [i] = 0;
         }
 
         i = 0;
         while (*ptr && *ptr != ':') {
-            buf_old [i] = *ptr;
+            bufs.buf_old [i] = *ptr;
             i++;
             ptr++;
         }
         ptr++; /* Skip over the colon*/
         i = 0;
         while (*ptr) {
-            buf_exp [i] = *ptr;
+            bufs.buf_exp [i] = *ptr;
             ptr++;
             i++;
         }
-        ptr = NULL;
+        free (line);
+        return TRUE;
+    } else {
+        /* End of file */
+        free (line);
+        return FALSE;
+    }
+}
+
+int check (char * filename)
+{
+    assert (filename);
+    FILE * fp = fopen (filename, "r");
+    if (NULL == fp) {
+        return -1;
+    }
+    bool ret;
+    while (((ret = get_single_entry (fp)) == TRUE) || (ret == IGNORE)) {
+        if (bufs.buf_old [0] == '#') {
+            /* Ignore comment line */
+            continue;
+        }
         test_count ++;
-        d_printf ("buf_old = [%s] buf_exp = [%s]\n", buf_old, buf_exp);
-        d_printf ("Running test #%d, input name is [%s]\n", test_count, buf_old);
+        d_printf ("buf_old = [%s] buf_exp = [%s]\n", bufs.buf_old, bufs.buf_exp);
+        d_printf ("Running test #%d, input name is [%s]\n", test_count, bufs.buf_old);
 
         /* Now, run the check */
-        char * actual_result = new_name (buf_old);
-        if (0 == strcmp (actual_result, buf_exp)) {
+        char * actual_result = new_name (bufs.buf_old);
+        if (0 == strcmp (actual_result, bufs.buf_exp)) {
             continue;
         } else {
             failcount++;
             if (check_type == SOFT_CHECK) {
                 fprintf (stderr, "Expected new name: [%s], Actual : [%s]\n",
-                         buf_exp, actual_result);
+                         bufs.buf_exp, actual_result);
                 return -1;
             } else {
                 assert (failcount == 1);
                 fprintf (stderr, "Expected new name: %s, Actual : %s\n",
-                         buf_exp, actual_result);
+                         bufs.buf_exp, actual_result);
                 fprintf (stderr, "%d/%d tests passed\n", (test_count - 1), test_count);
                 exit (-1);
             }
@@ -80,7 +109,6 @@ int check (char * filename)
     }
     d_printf ("%d strings found\n", test_count);
     fclose (fp);
-    free (line);
     return test_count;
 }
 
